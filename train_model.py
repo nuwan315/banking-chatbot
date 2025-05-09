744544f45c596b5197a15e4ab246376e8272bb5c
Nuwan Lakshan Perera <141323392+nuwan315@users.noreply.github.com>
2025-05-10 02:34:56 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,21 +1,23 @@
 import numpy as np
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D
+from tensorflow.keras.layers import Dense, Embedding, GlobalAveragePooling1D, Dropout
 from preprocess import preprocess_data
 
 # Load processed data
 X_train, y_train, output_dim, _, _ = preprocess_data()
 
 model = Sequential()
-model.add(Embedding(input_dim=1000, output_dim=16, input_length=10))
+model.add(Embedding(input_dim=2000, output_dim=32, input_length=20))
 model.add(GlobalAveragePooling1D())
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.3))
 model.add(Dense(16, activation='relu'))
 model.add(Dense(output_dim, activation='softmax'))
 
 model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
-model.fit(X_train, np.array(y_train), epochs=200)
+model.fit(X_train, np.array(y_train), epochs=300, batch_size=8, verbose=1)
 
 model.save("chat_model.h5")
-print("Model trained and saved.")
+print("✅ Model trained and saved.")
